125e480b178758195f345b06e6e9198d590c0f1e
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-09 06:32:16 -0500

    starting to debug cursor05.py

--- MatthewRandEmmaY/Cursor05.py
+++ MatthewRandEmmaY/Cursor05.py
@@ -15,32 +15,29 @@ import sys
 import os.path
 
 output = r"D:\Semester2\gis4207_Customization_I\day05\Output"
-if not os.path.exists(output):
-    os.makedirs(output)
-    print (" ")
-    print "**OUTPUT FOLDER CREATED**"
-else:
-    print (" ")
-    print "**OUTPUT FOLDER ALREADY EXISTS**"
+##if not os.path.exists(output):
+##    os.makedirs(output)
+##    print (" ")
+##    print "**OUTPUT FOLDER CREATED**"
+##else:
+##    print (" ")
+##    print "**OUTPUT FOLDER ALREADY EXISTS**"
 
-def main():
-    fc = r"..\..\..\Data\Canada\Can_Mjr_Cities.shp"
-    fields = ['NAME', 'PROV', 'SHAPE@X', 'SHAPE@Y','UTM_MAP']
-    count = 0
-    dscCS = arcpy.Describe(fc).spatialReference
-    print("Coordinate System:      " + dscCS.Name)
-    print  "Name, Province, Longitude, Latitude, UTM"
-    print getHeader()
-    for row in arcpy.da.SearchCursor(fc, fields):
-        count += 1
-        print getBody(row)
+##def main():
+fc = r"..\..\..\Data\Canada\Can_Mjr_Cities.shp"
+fields = ['NAME', 'PROV', 'SHAPE@X', 'SHAPE@Y','UTM_MAP']
+print  "Name, Province, Longitude, Latitude, UTM"
+getHeader()
+for row in arcpy.da.SearchCursor(fc, fields):
+    print getBody(row)
         #print(u'{0}, {1}, {2}, {3}, {4}'.format(row[0], row[1], row[2], row[3], row[4]))
-    print "There are {} cities in the above list".format(count)
+
 
 
 def getHeader():
     print """<?xml version="1.0" encoding="UTF-8"?>"""
     print """<kml xmlns="http://www.opengis.net/kml/2.2">"""
+    print "<Document>"
 
 
 def getBody(row):
@@ -53,15 +50,15 @@ def getBody(row):
     kml += " </Placemark>"
     kml +="</kml>"
     return kml
+
+
 #write to kml file
 ##savePath = output
 ##fileName = "Cities"
 ##completName = os.path.join(savePath,fileName+".kml")
 ##file1 = open(completName,"w")
-##tofile = getHeader
-##toFiletwo = getBody
-##file1.write(tofile)
+##file1.write(kml)
 ##file1.close()
 
-if __name__ == "__main__":
-    main()
+##if __name__ == "__main__":
+##    main()
